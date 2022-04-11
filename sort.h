c972cd827440fd5d458df3b9c248d96a0d42708f
Nasri Ibrahim <99497859+Nasriib@users.noreply.github.com>
2022-04-10 23:37:09 -0400

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,12 @@
+#ifndef ISIP_EXAMPLE
+#define ISIP_EXAMPLE
+//Include files
+//
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+//Defining max length
+//
+#define MAX_LINE_LENGTH 1000000
+
+#endif
